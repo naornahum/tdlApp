81fb9e616d54bdba0ad77d537f571a153a77cd09
naornahum <naornahum@cyber4s.dev>
2022-05-28 21:58:57 +0300

    bug fix

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,20 @@
-window.onload = function() {
-  const addButton : HTMLElement | null = document.getElementById("add-button");
-  const addInput : HTMLInputElement = (document.getElementById("new-task")as HTMLInputElement);
-  const tasks : HTMLElement | null = document.getElementById("tasks");
+window.onload = function () {
+  const addButton: HTMLElement | null = document.getElementById("add-button");
+  const addInput: HTMLInputElement = document.getElementById(
+    "new-task"
+  ) as HTMLInputElement;
+  const tasks: HTMLElement | null = document.getElementById("tasks");
 
-  if (addButton){
-    addButton.addEventListener("click", (_)=>{
+  if (addButton) {
+    addButton.addEventListener("click", (_) => {
       console.log(addInput.value);
       const id = Date.now();
-      window.localStorage.setItem(id.toString(), addInput.value)
-      const newTask = document.createElement("div");
+      window.localStorage.setItem(id.toString(), addInput.value);
+      console.log(tasks);
+      const newTask = document.createElement("li");
       const newTaskContent = document.createTextNode(addInput.value);
-      newTask.appendChild(newTaskContent)
-      tasks?.appendChild(newTask)
-    })
+      newTask.appendChild(newTaskContent);
+      tasks?.appendChild(newTask);
+    });
   }
-
-
 };
