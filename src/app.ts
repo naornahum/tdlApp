3a0e8c8c29cff602e64dcebd322a4b5c8fbf0bb6
naornahum <naornahum@cyber4s.dev>
2022-05-28 21:43:22 +0300

    commit

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+window.onload = function() {
+  const addButton : HTMLElement | null = document.getElementById("add-button");
+  const addInput : HTMLInputElement = (document.getElementById("new-task")as HTMLInputElement);
+  const tasks : HTMLElement | null = document.getElementById("tasks");
+
+  if (addButton){
+    addButton.addEventListener("click", (_)=>{
+      console.log(addInput.value);
+      const id = Date.now();
+      window.localStorage.setItem(id.toString(), addInput.value)
+      const newTask = document.createElement("div");
+      const newTaskContent = document.createTextNode(addInput.value);
+      newTask.appendChild(newTaskContent)
+      tasks?.appendChild(newTask)
+    })
+  }
+
+
+};
